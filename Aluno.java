9a79b50915c266b02d3a56d9ba0b614337719dc4
Gaby <gaabyrocha574@gmail.com>
2021-11-04 18:35:11 +0000

    revisão

--- Aluno.java
+++ Aluno.java
@@ -1,25 +1,40 @@
 public class Aluno extends Pessoa{
+    
+    private String cursos;
+    private double nota1;
+    private double nota2;
 
-    private double cursos; 
-    private String notas;
-
-
-    public void setCursos(double cursos){
+    public void setCursos(String cursos){
         this.cursos = cursos;
-    }public double getCursos(){
+    }public String cursos(){
         return this.cursos;
     }
-    public void setNotas(double notas){
-        this.notas = notas;
-    }public double getNotas(){
-        return this.notas;
+
+    public void setNota1(double nota1){
+        this.nota1 = nota1;
+    }public double getNota1(){
+        return this.nota1;
     }
-    
 
+    public void setNota2(double nota2){
+        this.nota2 = nota2;
+    }public double getNota2(){
+        return this.nota2;
+    }
 
+    public double calcularMedia(){
+        return (this.nota1 + this.nota2)/2.0;
+    }
+
+    public boolean verificarAprovado(){
+        if(calcularMedia() < 6.0){
+            System.out.println("Aluno reprovado");
+            return false;
+        }else{
+            System.out.println("Aluno aprovado");
+            return true;
+        }
+    }
 }
 
-public double calculaMedia(){
-    return this.notas;
-}
 
