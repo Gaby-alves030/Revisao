69a2819b5f9e5a1985961a0be2e59d3248aef2a8
Gaby <gaabyrocha574@gmail.com>
2021-11-10 22:22:41 +0000

    herança

--- Aluno.java
+++ Aluno.java
@@ -1,40 +1,50 @@
-public class Aluno extends Pessoa{
-    
-    private String cursos;
+public class Aluno extends Pessoa {
+
     private double nota1;
     private double nota2;
-
-    public void setCursos(String cursos){
-        this.cursos = cursos;
-    }public String cursos(){
-        return this.cursos;
-    }
+    private String cursos;
 
     public void setNota1(double nota1){
         this.nota1 = nota1;
-    }public double getNota1(){
+    }
+
+    public double getNota1(){
         return this.nota1;
     }
 
     public void setNota2(double nota2){
         this.nota2 = nota2;
-    }public double getNota2(){
+    }
+
+    public double getNota2(){
         return this.nota2;
     }
 
-    public double calcularMedia(){
-        return (this.nota1 + this.nota2)/2.0;
+    public void setCursos(String cursos){
+        this.cursos = cursos;
     }
 
-    public boolean verificarAprovado(){
-        if(calcularMedia() < 6.0){
-            System.out.println("Aluno reprovado");
-            return false;
+    public String getCursos(){
+        return this.cursos;
+    }
+
+
+    public double calculaMedia(){
+
+        return (this.nota1 + this.nota2) / 2.0;
+        
+    }
+
+    public boolean verificaAprovado(){
+
+        boolean condicao;
+
+        if(calculaMedia() < 6.0){
+            condicao = false;
         }else{
-            System.out.println("Aluno aprovado");
-            return true;
+            condicao = true;
         }
+
+        return condicao;
     }
 }
-
-
